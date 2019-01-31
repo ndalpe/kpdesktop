2617dbb58500988e9f17f4c95d70038551a0f8cd
ndalpe <ndalpe@gmail.com>
2019-01-31 09:12:29 +0700

    Select all course by default when creating new user

--- javascript/pagelayout_course.js
+++ javascript/pagelayout_course.js
@@ -4,6 +4,8 @@ require(['jquery'], function($) {
 if ($("body.pagelayout-course").length > 0) {
 	var actionTitle = '';
 
+	/******************************************/
+	/**** Replace lesson and activity icon ****/
 	$.each(['lesson', 'quiz'], function( index, value ) {
 		$("li.activity."+value).each(function(){
 			actionTitle = $(this).find('span.actions img').attr('title');
@@ -21,4 +23,13 @@ if ($("body.pagelayout-course").length > 0) {
 		});
 	});
 }
+
+	/*************************************************************/
+	/**** Select all course by default when creating new user ****/
+	/**** blocks/iomad_company_admin/company_user_create_form.php ****/
+if ($("#page-blocks-iomad_company_admin-company_user_create_form #currentcourses").length > 0) {
+	// selecting all the course in the Essential category
+	$("#currentcourses").val([7,9,10,11,12,13,14,15,16,17,18]).change();
+}
+
 });
